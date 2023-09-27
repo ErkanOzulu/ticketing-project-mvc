fc8af02902c8750097243c574580db826ba493da
Erkan <ErkanOzulu@users.noreply.github.com>
2023-09-27 12:22:37 +0300

    Initial commit

---
+++ src/main/java/com/cydeo/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = {"/login","/"}) //localhost:8080/login or localhost:8080/
+    public String login(){
+
+        return "login";
+    }
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
+}
