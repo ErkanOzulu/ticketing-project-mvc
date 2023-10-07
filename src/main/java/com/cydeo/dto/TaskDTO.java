3197fc1d4ce07348c8a3e25d5f0d78537c873df5
Erkan <ErkanOzulu@users.noreply.github.com>
2023-10-07 18:48:29 +0300

    TaskDTO

--- src/main/java/com/cydeo/dto/TaskDTO.java
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -13,20 +13,20 @@ import java.util.UUID;
 public class TaskDTO {
     private Long id;
     private ProjectDTO project;
-    private UserDTO user;
+    private UserDTO assignedEmployee;
     private String taskSubject;
     private  String taskDetail;
-    private LocalDate assignedTask;
+    private LocalDate  assignedDate;
     private Status taskStatus;
 
-    public TaskDTO(ProjectDTO project, UserDTO user, String taskSubject, String taskDetail, LocalDate assignedTask, Status taskStatus) {
+    public TaskDTO(ProjectDTO project, UserDTO assignedEmployee, String taskSubject, String taskDetail, LocalDate assignedDate, Status taskStatus) {
         this.project = project;
-        this.user = user;
+        this.assignedEmployee = assignedEmployee;
         this.taskSubject = taskSubject;
         this.taskDetail = taskDetail;
-        this.assignedTask = assignedTask;
+        this.assignedDate = assignedDate;
         this.taskStatus = taskStatus;
-        this.id= UUID.randomUUID().getMostSignificantBits();
+        this.id = UUID.randomUUID().getMostSignificantBits();
 
     }
 }
