d38bb40f9078bd5884e0b1aeb6c6d80eb15d0c00
Erkan <ErkanOzulu@users.noreply.github.com>
2023-10-07 20:45:25 +0300

    Part5

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -40,4 +40,10 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
      return super.findAll().stream().filter(u->u.getRole().getId()==2).collect(Collectors.toList());
 
     }
+
+    @Override
+    public List<UserDTO> findEmployees() {
+        return super.findAll().stream().filter(user -> user.getRole().getId() == 3).collect(Collectors.toList());
+
+    }
 }
